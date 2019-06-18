34ae06e92f0d97694ea78f21cb7f1d1cf41360cc
Justin Heyes-Jones <justinhj@gmail.com>
2019-06-18 13:49:19 -0700

    It works lol

--- src/main/scala/org/justinhj/Comonad.scala
+++ src/main/scala/org/justinhj/Comonad.scala
@@ -19,7 +19,25 @@ object Comonad {
   // duplicate gives all the foci
   // so we can use coflatmap to get the sum of the neighbours etc
 
-  case class FocusedGrid[A](focus: Tuple2[Int,Int], grid : Vector[Vector[A]]) 
+  case class FocusedGrid[A](focus: Tuple2[Int,Int], grid : Vector[Vector[A]])
+
+  // Get the sum of the focus and the values around it
+  def localSum(fg : FocusedGrid[Int]) : Int = {
+    def getAt(point : Tuple2[Int,Int]) : Int = {
+      val what = fg.grid.get(point._1).flatMap(row => row.get(point._2))
+      what.getOrElse(0)
+    }
+    
+    getAt((fg.focus._1 - 1, fg.focus._2 - 1)) +
+    getAt((fg.focus._1, fg.focus._2 - 1)) +
+    getAt((fg.focus._1 + 1, fg.focus._2 - 1)) +
+    getAt((fg.focus._1 - 1, fg.focus._2)) +
+    getAt((fg.focus._1, fg.focus._2)) +
+    getAt((fg.focus._1 + 1, fg.focus._2)) +
+    getAt((fg.focus._1 - 1, fg.focus._2 + 1)) +
+    getAt((fg.focus._1, fg.focus._2 + 1)) +
+    getAt((fg.focus._1 + 1, fg.focus._2 + 1))
+  }
   
   implicit val focusedGridComonad = new Comonad[FocusedGrid] {
 
@@ -57,6 +75,9 @@ object Comonad {
 
   val fg1 = FocusedGrid((1,1), Vector(g1,g2,g3))
 
+  //@ fg1.coflatMap(localSum(_)) 
+  // LOL!
+  //res6: FocusedGrid[Int] = FocusedGrid((1, 1), Vector(Vector(12, 21, 16), Vector(27, 45, 33), Vector(24, 39, 28)))
 
   def main(args : Array[String]) : Unit = {
     println("Hello, mate!")
