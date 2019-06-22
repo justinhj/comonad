f349d706edee77eeb3ed892b51814a436b90b44d
Justin Heyes-Jones <justinhj@gmail.com>
2019-06-22 12:47:23 -0700

    blur wokring

--- src/main/scala/org/justinhj/ImageProcessor.scala
+++ src/main/scala/org/justinhj/ImageProcessor.scala
@@ -14,20 +14,19 @@ object ImageProcessor {
   import FocusedGrid._
 
   // Safely get the value at the focus, returning a default value if we go off the grid
-  def getAt(fg : FocusedGrid[Int], point : Tuple2[Int,Int], default: Int) : Int = {
-    fg.grid.get(fg.focus._1).flatMap(row => row.get(fg.focus._2)).getOrElse(default)
+  def getAt(fg : FocusedGrid[(Int,Int,Int)], point : Tuple2[Int,Int], default: (Int,Int,Int)) : (Int,Int,Int) = {
+    fg.grid.get(point._1).flatMap(row => row.get(point._2)).getOrElse(default)
   }
 
   // Get the sum of the values around the focus
-  def localSum(fg : FocusedGrid[Int], default: Int) : Int = {  
-    val points = List(-1,0,1)
-    Applicative[List].map2(points,points){case (a : Tuple2[Int,Int]) => identity(a)}.
-      map(coord => getAt(fg, coord |+| fg.focus, default)).sum
+  def localSum(fg : FocusedGrid[(Int,Int,Int)], default: (Int,Int,Int)) : (Int,Int,Int) = {  
+    val points = List(-10,-9,-8,-7,-6,-5,-4,-3,-2,-1,0,1,2,3,4,5,6,7,8,9,10)
+    Applicative[List].map2(points,points){case (r,c) => getAt(fg, (r,c) |+| fg.focus, default)}.combineAll
   }
 
 // Copies the image data from the focused grid back to the image
 // Note the image is mutable here
-def focusedGridToImage(fg : FocusedGrid[Int], img: BufferedImage) : Unit = {
+def focusedGridToImage(fg : FocusedGrid[(Int,Int,Int)], img: BufferedImage) : Unit = {
   val w = img.getWidth
   val h = img.getHeight
 
@@ -36,26 +35,35 @@ def focusedGridToImage(fg : FocusedGrid[Int], img: BufferedImage) : Unit = {
 
   for (x <- 0 until w)
     for (y <- 0 until h) {
-      img.setRGB(x,y,fg.grid(y)(x))
+      val rgb = fg.grid(y)(x)
+      img.setRGB(x,y,rgb._1 << 16 | rgb._2 << 8 | rgb._3)
     }
 }
 
-def imageToFocusedGrid(img: BufferedImage) : FocusedGrid[Int] = {
+def colourToTuple(rgb: Int) : (Int,Int,Int) = {
+  val red = (rgb >> 16) & 0x000000FF
+  val green = (rgb >>8 ) & 0x000000FF
+  val blue = (rgb) & 0x000000FF
+
+  (red, green, blue)
+}
+
+def imageToFocusedGrid(img: BufferedImage) : FocusedGrid[(Int,Int,Int)] = {
   val w = img.getWidth
   val h = img.getHeight
 
-  val grid = (0 until h).foldLeft(Vector.empty[Vector[Int]]) {
+  val grid = (0 until h).foldLeft(Vector.empty[Vector[(Int,Int,Int)]]) {
     case (acc1, y) =>
-      acc1 :+ (0 until w).foldLeft(Vector.empty[Int]) {
+      acc1 :+ (0 until w).foldLeft(Vector.empty[(Int,Int,Int)]) {
       case (acc, x) =>
-        acc :+ (img.getRGB(x,y) & 0xff)
+        acc :+ colourToTuple(img.getRGB(x,y))
     }
   }
 
   FocusedGrid((0,0), grid)
 }   
 
-def processImage(img: BufferedImage, f: FocusedGrid[Int] => Int): BufferedImage = {
+def processImage(img: BufferedImage, f: FocusedGrid[(Int,Int,Int)] => (Int,Int,Int)): BufferedImage = {
   val w = img.getWidth
   val h = img.getHeight
 
@@ -67,18 +75,6 @@ def processImage(img: BufferedImage, f: FocusedGrid[Int] => Int): BufferedImage 
     for (y <- 0 until h) {
       val rgb = img.getRGB(x,y)
       out.setRGB(x,y,rgb)
-    }
-
-  // Convert the image to a grey scale 
-  for (x <- 0 until w)
-    for (y <- 0 until h) {
-      val rgb = img.getRGB(x,y)
-      val red = (((rgb >> 16) & 0x000000FF) * 0.3).toInt
-      val green = (((rgb >>8 ) & 0x000000FF) * 0.59).toInt
-      val blue = (((rgb) & 0x000000FF) * 0.11).toInt
-      val brightness = (red + blue + green)/3
-      val newRGB = (brightness << 16) | (brightness << 8) | brightness
-      out.setRGB(x,y,newRGB)
     }
 
     val fg = imageToFocusedGrid(out)
@@ -97,9 +93,9 @@ def processImage(img: BufferedImage, f: FocusedGrid[Int] => Int): BufferedImage 
 
     println(photo1.getHeight())
         
-    def boxFilter(fg: FocusedGrid[Int]) : Int = {
-      val sum = localSum(fg, 1)
-      (sum / 9.0).toInt
+    def boxFilter(fg: FocusedGrid[(Int,Int,Int)]) : (Int,Int,Int) = {
+      val sum = localSum(fg, (255,255,255))
+      ((sum._1 / 441.0).toInt, (sum._2 / 441.0).toInt, (sum._3 / 441.0).toInt)
     }
 
     val photo2 = processImage(photo1, boxFilter) 
