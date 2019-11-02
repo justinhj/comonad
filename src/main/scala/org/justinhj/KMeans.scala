a838e889917f2f1132dd0999991b6217d3f9e85b
Justin Heyes-Jones <justinhj@gmail.com>
2019-11-02 12:01:40 -0700

    abandoned as this is not a good use of comonad operations

--- src/main/scala/org/justinhj/KMeans.scala
+++ src/main/scala/org/justinhj/KMeans.scala
@@ -1,6 +1,24 @@
 package org.justinhj
 
 object KMeans {
+
+/*
+// Initialize centroids
+// X is the data set and K is the number of centroids
+centroids = kMeansInitCentroids(X, K);
+for iter = 1:iterations
+    // Cluster assignment step: Assign each data point to the
+    // closest centroid. idx(i) corresponds to cˆ(i), the index
+    // of the centroid assigned to example i
+    idx = findClosestCentroids(X, centroids);
+    // Move centroid step: Compute means based on centroid
+    // assignments
+    centroids = computeMeans(X, idx, K);
+end
+*/
+
+
+
     def main(args : Array[String]) : Unit = {
         println("Tup?")
     }
